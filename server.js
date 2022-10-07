e26ef256f27fa37c7f3780b71cfa71b67ec19643
msi-ism <msism720@gmail.com>
2022-10-06 20:26:13 -0400

    Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express')
 
-
-
+const pokemon = require('./models/pokemon')
 
 const app = express()
 const PORT = 3000
 
-
+// ^ Root Route
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
-
+// ^ Pokemon json route
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
+})
 
 
 app.listen(PORT, () => {
